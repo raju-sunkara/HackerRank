1a77d4858550b158c1fc632ced9140d160a86f3b
raju-sunkara <raju.sunkara@gmail.com>
2022-05-01 18:32:54 +0530

    office lap changes 5_1

---
+++ test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args) {
+
+        double a= 120.12;
+        System.out.println(String.format("%2.0f", a));
+        //TODO an extra change 1.0
+        System.out.println("Feature1 added ");
+        System.out.println("Feature2 added ");
+    }
+}
