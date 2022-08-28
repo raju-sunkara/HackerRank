1676246ff513f5118f02b439988812b35f95a2ba
raju sunkara <raju.sunkara@gmail.com>
2022-08-28 21:42:55 +0530

    Update 28-08-2022 update2

--- CodeFiles/PrintStars.java
+++ CodeFiles/PrintStars.java
@@ -10,12 +10,12 @@ public class PrintStars {
         int lines = sc.nextInt();
         int it=0;
         for(int l=0;l<lines;l++){//to print lines
-            System.out.print(l+ " "+it);
-            for(it=0;it<lines-l;it++){
-                System.out.print(" ");
+            //System.out.print(l+ " "+it);
+            for(int k=0;k<lines-l-1;k++){
+                System.out.print("   ");
             }
-            for(it=l;it<lines;it++){
-                System.out.print("*");
+            for(it=0;it<l;it++){
+                System.out.print("  *  ");
             }
             System.out.println("");
         }
